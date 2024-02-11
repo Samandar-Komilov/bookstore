d1aeccede6cf75f6fb59283af4a222899a4df8dd
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-02-11 21:25:52 +0500

    Custom User Form, tests

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'bookstore',
         'USER': 'postgres',
         'PASSWORD': 'voidpointer',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': 5432
     }
 }
